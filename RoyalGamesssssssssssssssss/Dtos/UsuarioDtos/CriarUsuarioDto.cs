a8c24804db6863d8b418fe65e60b8ccff8a84d20
LUIGI SANTI BERLATO <42344828893@info.local>
2026-03-02 08:34:09 -0300

    Correções

--- RoyalGamesssssssssssssssss/Dtos/UsuarioDtos/CriarUsuarioDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RoyalGames.Dtos.UsuarioDtos
-{
-    public class CriarUsuarioDto
-    {
-        public string Nome {  get; set; }
-        public string Email {  get; set; }
-        public string senha { get; set; }
-    }
-}
